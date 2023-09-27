4d96b858cb2a5424b3939ebffc43189ec83abb7e
Asad Awan <asadawn3@gmail.com>
2023-09-27 19:55:51 +0500

    setup tailwind and make protected routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
+import ForgetPassword from "./components/ForgetPassword";
+import PrivateComponent from "./components/PrivateComponent";
+import About from "./components/About";
 function App() {
+  console.log("data", localStorage.getItem("user"));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PrivateComponent />}>
+            <Route path="/" element={<About />} />
+            <Route path="/logout" element={<h1>Logout Component</h1>} />
+            <Route path="/profile" element={<h1>Profile Component</h1>} />
+          </Route>
+
+          <Route path="/forget" element={<ForgetPassword />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/ProtectedRoutes.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import { Route } from "react-router-dom";
+// import PrivateComponent from "./PrivateComponent";
+// import ForgetPassword from "./ForgetPassword";
+
+// const ProtectedRoutes = () => {
+//   return (
+//     <Route element={<PrivateComponent />}>
+//       <Route path="/" element={<h1>Home Componet</h1>} />
+//       <Route path="/forget" element={<ForgetPassword />} />
+//     </Route>
+//   );
+// };
+
+// export default ProtectedRoutes;
